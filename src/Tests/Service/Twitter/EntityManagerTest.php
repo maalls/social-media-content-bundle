374117d03b43ab1868a4cf6e8492a38cf35af0ec
maalls <maalls@gmail.com>
2018-04-13 18:23:32 +0900

    Add entity manager for twitter api.

---
+++ src/Tests/Service/Twitter/EntityManagerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Maalls\SocialMediaContentBundle\Tests\Service\Twitter;
+
+class EntityManagerTest extends \Maalls\SocialMediaContentBundle\Tests\KernelTestCase 
+{
+
+    public function testUpdateTimeline()
+    {
+
+        $this->assertEqual(true, true);
+
+    }
+
+
+}
